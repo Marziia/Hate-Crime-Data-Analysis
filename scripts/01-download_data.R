6c54308e02fb0f8ee689d8c5cf126fed67a85262
Marziia <146518323+Marziia@users.noreply.github.com>
2024-09-26 05:13:31 -0700

    Updated the download script to allow downloads from open data Toronto

--- scripts/01-download_data.R
+++ scripts/01-download_data.R
@@ -1,26 +1,34 @@
 #### Preamble ####
-# Purpose: Downloads and saves the data from [...UPDATE THIS...]
+# Purpose: Downloads and saves the data from the Open Data Toronto Database
 # Author: Rohan Alexander [...UPDATE THIS...]
-# Date: 11 February 2023 [...UPDATE THIS...]
+# Date: 26 September 2024
 # Contact: rohan.alexander@utoronto.ca [...UPDATE THIS...]
 # License: MIT
-# Pre-requisites: [...UPDATE THIS...]
-# Any other information needed? [...UPDATE THIS...]
+# Pre-requisites: opendatatoronto, tidyverse, dplyr libraries installed
+# Any other information needed? Name of the data set to be downloaded
 
 
 #### Workspace setup ####
 library(opendatatoronto)
 library(tidyverse)
-# [...UPDATE THIS...]
+library(dplyr)
+nameOfDataSet <- "hate-crimes-open-data"
 
 #### Download data ####
-# [...ADD CODE HERE TO DOWNLOAD...]
+package <- show_package(nameOfDataSet)
+package
 
+# get all resources for this package
+resources <- list_package_resources(nameOfDataSet)
+
+# identify datastore resources; by default, Toronto Open Data sets datastore resource format to CSV for non-geospatial and GeoJSON for geospatial resources
+
+datastore_resources <- filter(resources, tolower(format) %in% c('csv', 'geojson'))
+
+# load the first datastore resource as a sample
+data <- filter(datastore_resources, row_number()==1) %>% get_resource()
 
 
 #### Save data ####
-# [...UPDATE THIS...]
-# change the_raw_data to whatever name you assigned when you downloaded it.
-write_csv(the_raw_data, "inputs/data/raw_data.csv") 
+write_csv(data, "inputs/data/raw_data.csv") 
 
-         
